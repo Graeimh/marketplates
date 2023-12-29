38cc292166eb5ce1c6020cbc2e47ca1c35551414
Graiemh <qg.guinier@gmail.com>
2023-12-29 23:33:56 +0100

    Font awesome installation start of html-css work

--- marketplates/src/components/AdminDashboard/Dashboard/Dashboard.tsx
+++ marketplates/src/components/AdminDashboard/Dashboard/Dashboard.tsx
@@ -1,18 +1,44 @@
 import { Link } from "react-router-dom";
+import styles from "./Dashboard.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 function Dashboard() {
   return (
     <>
-      <h1>Hi! Welcome to the dashboard!</h1>
-      <ul>
+      <ul id={styles.dashboardPanel}>
         <li>
-          <Link to="/users">Manipulate users</Link>
+          <Link to="/users">
+            <span className={styles.dashboardOptionChevron}>
+              <FontAwesomeIcon icon={solid("chevron-right")} />
+            </span>
+            <span className={styles.dashboardOptionText}>Manipulate users</span>
+            <span className={styles.dashboardOptionDecorator}>
+              <FontAwesomeIcon icon={solid("users")} />
+            </span>
+          </Link>
         </li>
         <li>
-          <Link to="/tags">Manipulate tags</Link>
+          <Link to="/tags">
+            <span className={styles.dashboardOptionChevron}>
+              <FontAwesomeIcon icon={solid("chevron-right")} />
+            </span>
+            Manipulate tags
+            <span className={styles.dashboardOptionDecorator}>
+              <FontAwesomeIcon icon={solid("tags")} />
+            </span>
+          </Link>
         </li>
         <li>
-          <Link to="/places">Manipulate places</Link>
+          <Link to="/places">
+            <span className={styles.dashboardOptionChevron}>
+              <FontAwesomeIcon icon={solid("chevron-right")} />
+            </span>
+            Manipulate places
+            <span className={styles.dashboardOptionDecorator}>
+              <FontAwesomeIcon icon={solid("shop")} />
+            </span>
+          </Link>
         </li>
       </ul>
     </>
--- marketplates/src/components/Layouts/LayoutForms/LayoutForms.tsx
+++ marketplates/src/components/Layouts/LayoutForms/LayoutForms.tsx
@@ -1,17 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Outlet, useNavigate } from "react-router-dom";
 import styles from "./LayoutForms.module.scss";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const LayoutForms = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <button type="button" onClick={() => navigate(-1)}>
-        {"<"}
-      </button>
-      <h1>New layout!</h1>
-
-      <Outlet />
+      <div id={styles.formsContainer}>
+        <button
+          type="button"
+          onClick={() => navigate(-1)}
+          id={styles.formsButton}
+        >
+          <FontAwesomeIcon icon={solid("rotate-left")} />
+        </button>
+        <img
+          src="https://storage.needpix.com/rsynced_images/kitchen-85270_1280.jpg"
+          alt="pots and pans"
+          id={styles.formsImage}
+        />
+        <div id={styles.formsContentContainer}>
+          <div id={styles.formsContent}>
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
